7d77edd56f83aac533f41c127eda86be53094b6c
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-30 21:50:26 +0900

    feat: 프로젝트 홈에 들어갈 projectApi.js 내 메서드 생성함

--- src/apis/projectApi.js
+++ src/apis/projectApi.js
@@ -5,7 +5,23 @@ function getProjectList(){
   return axios.get("/api/project/list");
 }
 
+function getProjectDetail(projectId){
+  return axios.get("/api/project/detail",{params:{projectId}});
+}
+
+function getProjectSchedules(projectId){
+  return axios.get("/api/project/schedules",{params:{projectId}})
+}
+
+function getProjectMembers(projectId){
+  return axios.get("/api/project/userprojectroles",{params:{projectId}})
+}
+
 
 export default{
-  getProjectList
+  getProjectList,
+  getProjectDetail,
+  getProjectSchedules,
+  getProjectMembers
+
 }
