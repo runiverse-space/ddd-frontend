74f7275d233802d6b889b868cfb76e5717616ad0
dlgudwn55 <dkrlekeheoek@naver.com>
2025-09-30 18:33:55 +0900

    fix: usersApi 복구

--- src/apis/usersApi.js
+++ src/apis/usersApi.js
@@ -1,5 +1,29 @@
 import axios from "axios";
+import "./axiosConfig";
 
-export default{
-  
+function usersCreate(users) {
+  return axios.post("/api/users/create", users);
 }
+
+function usersLogin(loginForm) {
+  return axios.post("/api/users/login", loginForm);
+}
+
+function usersInfo(userId) {
+  return axios.get("/api/users/detail/id", {
+    params: {userId},
+  });
+}
+
+function ufAttachDownload(userId) {
+  return axios.get("/api/users/attach-download", {params: {userId}, responseType: "blob"});
+}
+
+const usersApi = {
+  usersCreate,
+  usersLogin,
+  usersInfo,
+  ufAttachDownload
+};
+
+export default usersApi;
