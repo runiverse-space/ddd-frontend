a2a1756ffc0f8e36ec1b6cb9426f1220f103af60
Addilete <165387997+adillette@users.noreply.github.com>
2025-10-01 17:20:01 +0900

    chore: 공통 메서드에 맞게 메서드 이름 변경함

--- src/apis/knowledgeApi.js
+++ src/apis/knowledgeApi.js
@@ -1,18 +1,33 @@
 import axios from "axios";
 import "./axiosConfig";
 
-function getKnowledgeList(projectId){
+function knowledgeList(projectId){
   return axios.get("/api/knowledge/list",{params:{projectId}})
 }
 
-function createKnowledge(formData){
+function knowledgeCreate(formData){
   return axios.post("/api/knowledge/create", formData);
+}
+
+function knowledgeUpdate(formData){
+  return axios.post("/api/knowledge/update", formData);
+}
+
+function knowledgeAttachDownload(knowledgeId){
+  return axios.get("/api/knowledge/attachdownload", {params:{knowledgeId}});
+}
+
+function knowledgeDetail(knowledgeId){
+  return axios.get("/api/knowledge/detail", {params:{knowledgeId}});
 }
 
 
 
 
 export default{
-  getKnowledgeList,
-  createKnowledge
+  knowledgeList,
+  knowledgeCreate,
+  knowledgeUpdate,
+  knowledgeAttachDownload,
+  knowledgeDetail
 }
