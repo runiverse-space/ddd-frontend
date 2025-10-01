c9a3376b21b95b2dc89d3bafb12f1765ab508efa
dlgudwn55 <dkrlekeheoek@naver.com>
2025-10-01 19:28:23 +0900

    feat: scheduleApi 작성

--- src/apis/scheduleApi.js
+++ src/apis/scheduleApi.js
@@ -1,5 +1,25 @@
 import axios from "axios";
+import "./axiosConfig";
+
+function getSchedule(scheduleId) {
+  return axios.get("/api/schedule/detail");
+}
+
+function getProjectScheduleList(projectId){
+  return axios.get("/api/project/schedules", {params:{projectId}});
+}
+
+function getScheduleMemberList(scheduleId) {
+  return axios.get("/api/schedule/users", {params: {scheduleId}});
+}
+
+function scheduleCreate(schedule) {
+  return axios.post("/api/schedule/create", schedule);
+}
 
 export default{
-  
+  getSchedule,
+  getProjectScheduleList,
+  getScheduleMemberList,
+  scheduleCreate
 }
