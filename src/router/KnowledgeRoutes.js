e6bc42182ce0ff4ea29bd85a6ae08577ed7115bb
Addilete <165387997+adillette@users.noreply.github.com>
2025-10-01 17:20:48 +0900

    feat: 지식창고 리스트 조회

--- src/router/KnowledgeRoutes.js
+++ src/router/KnowledgeRoutes.js
@@ -1,14 +1,28 @@
 const routes = [
   {
-    path: "knowledge",
+    path: "KnowledgeList",
     name: "KnowledgeHome",
-    component: () => import("@/views/projects/Knowledge/Knowledge.vue"),
+    component: () => import("@/views/projects/Knowledge/KnowledgeList.vue"),
     props: true,
   },
+  
   {
     path: "KnowledgeWrite",
     name: "KnowledgeWrite",
     component: () => import("@/views/projects/Knowledge/KnowledgeWrite.vue"),
+    props: true,
+  },
+
+  {
+    path: "KnowledgUpdate",
+    name: "KnowledgeWrite",
+    component: () => import("@/views/projects/Knowledge/KnowledgUpdate.vue"),
+    props: true,
+  },
+    {
+    path: "KnowledgeDetail",
+    name: "KnowledgeWrite",
+    component: () => import("@/views/projects/Knowledge/KnowledgeDetail.vue"),
     props: true,
   },
 ];
